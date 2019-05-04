abb6bd50e6bd6651822620c88b1d4f8ead189675
lucas404x <joselucas0303@gmail.com>
2019-05-04 18:21:28 -0300

    Arquivo para a execução do programa

--- main.py
+++ main.py
@@ -1,44 +1,8 @@
-import anagrama
-
-def main():
-
-    """
-    Função principal da aplicação
-    """
-
-    while True:
-        
-        print()
-        print("[1] - Calcular anagrama")
-        print("[2] - Sair")
-        print()
-        
-        opcao = int(input("Opção: "))
-
-        if opcao == 2:
-            break
-        
-        else:
-            
-            valor = int(input("Digite um número para o calculo: "))
-            print()
-            print('[1] - Exibir todos os valores')
-            print('[2] - Exibir o menor anagrama')
-            print('[3] - Exibir o maior anagrama')
-            print()
-            exibir = int(input("Opção: "))
-
-            if exibir is 1:
-                exibir = ''
-            if exibir is 2:
-                exibir = 'min'
-            if exibir is 3:
-                exibir = 'max'
-
-            print()
-            
-        print(anagrama.anagrama(valor, exibir))
+from tkinter import *
+from janela import Janela
 
 if __name__ == '__main__':
-
-    main()
+    
+    janela = Tk()
+    Janela(janela)
+    janela.mainloop()
