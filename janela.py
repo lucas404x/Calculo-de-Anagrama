c05e8a7981b892bd77a064c03b2edde598a56020
lucas404x <joselucas0303@gmail.com>
2019-05-04 23:32:58 -0300

    .

--- janela.py
+++ janela.py
@@ -1,26 +1,28 @@
 from tkinter import*
+from tkinter import messagebox, filedialog
 from functools import partial
 from anagrama import anagrama
 
 class Janela:
 
         def __init__(self, janela):
+
+                self.janelaPrincipal = janela
+                self.janelaPrincipal.title("Calcula Permutações")
+                self.janelaPrincipal.geometry("250x85")
+                self.janelaPrincipal.resizable(False, False)
+                self.windowResult = None
                 
-                self.janela = janela
-                self.janela.title("Calcula Permutações")
-                self.janela.geometry("250x85")
-                self.janela.resizable(False, False)
-                
-                self.info_inserir = Label(self.janela, text = "Insira um número ou nome")
-                self.inserir_dado = Entry(self.janela)
-                self.confirmar = Button(self.janela, text = "Calcular", foreground = 'black', command = self.calcularPermutacao)
+                self.info_inserir = Label(self.janelaPrincipal, text = "Insira um número ou nome", font = (14))
+                self.inserir_dado = Entry(self.janelaPrincipal)
+                self.confirmar = Button(self.janelaPrincipal, text = "Calcular", foreground = 'black', command = self.calcularPermutacao)
 
                 self.info_inserir.pack()
                 self.inserir_dado.pack()
                 self.confirmar.pack()
 
         def calcularPermutacao(self):
-
+                
                 calc = anagrama(self.inserir_dado.get())
                 text = ""
                 const = len(list(self.inserir_dado.get()))
@@ -34,21 +36,29 @@ class Janela:
                         else:
                                 text += calc[valor] + " |"
 
+                self.inserir_dado.delete(0, END)
                 self.exibirPermutacao(text)
 
         def exibirPermutacao(self, text_result):
 
-                windowResult = Tk()
-                windowResult.title("Resultado")
-                self.janela.resizable(False, False)
+                self.windowResult = Tk()
+                self.windowResult.title("Resultado")
                 
-                lb = Label(windowResult, text = text_result)
-                save = Button(windowResult, text = "Salvar Anagramas", command = partial(self.save, text_result))
-                sair = Button(windowResult, text = "Fechar", command = lambda:exit)
+                lb = Label(self.windowResult, text = text_result)
+                save = Button(self.windowResult, text = "Salvar Anagramas", command = partial(self.save, text_result))
+                sair = Button(self.windowResult, text = "Fechar", command = lambda:self.windowResult.destroy())
 
                 lb.pack()
                 save.pack(side = 'right')
                 sair.pack(side = 'left')
         
         def save(self, text):
-                pass
+
+                opcoes = {'defaultextension':'.txt', 'title':"Escolha um diretorio para salvar o Anagrama"}
+
+                try:
+                        arquivo = filedialog.asksaveasfile(mode = 'w', **opcoes)
+                        arquivo.write(text)
+                        arquivo.close()
+                except:
+                        print("Arquivo não foi salvo")
